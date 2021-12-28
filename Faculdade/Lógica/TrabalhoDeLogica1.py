848828d12dbafa88efdae340882255f11d017f22
Moisés Oliveira Costa <mosolicos@gmail.com>
2021-12-28 08:56:11 -0300

    Update TrabalhoDeLogica1.py

--- Faculdade/Lógica/TrabalhoDeLogica1.py
+++ Faculdade/Lógica/TrabalhoDeLogica1.py
@@ -1,60 +1,38 @@
-#Função para verificar se é fórmula ou não
-def principal(a, contsubf):
+### FUNÇÃO PARA VERIFICAR SE É UMA FORMULA OU NÃO
+def principal(a):
 
-    #Caso o número de abertura de parentese for diferente de fechamento de parentese
     if a.count("(") != a.count(")"):
         return "FALSO"
 
-    #Caso se só foi digitado um átomo e estiver em minúsculo
     if len(a) == 1 and a.islower() and a.isalpha():  
-        if a not in contsubf:   
-            contsubf.append(a)
         return "x"
 
-    #Caso tenha negação na fórmula
     elif "-" in a:
 
-        #Procurando o conectivo de negação
         x = a.find("-")
 
-        #Caso a negação esteja no fim da fórmula, sem um átomo depois
         if x == len(a)-1:
             return "FALSO"
 
-        #Caso, depois da negação, seja um átomo ou um parentese
         if a[x+1].isalpha() or a[x+1] == "(":
 
-            #Caso depois da negação for um átomo
-            if a[x+1].isalpha() and a[x]+a[x+1] not in contsubf:   
-                contsubf.append(a[x:x+2])
-            #Caso depois da negação for uma abertura de parentese
-            if a[x+1] == "(":   
-                contsubf.append(a)
-
             a = a[0:x]+a[x+1:]
-            return "-" + principal(a, contsubf)
+            return "-" + principal(a)
 
-        #Senão for nenhum desses casos
         else:
             return "FALSO"
             
     else:
 
-        #Caso não tenha parenteses na fórmula
         if "(" not in a and ")" not in a:
             return "FALSO"
 
-        else: 
+        elif a[0] == "(" and a[-1] == ")":
 
-            #Caso a fórmula não tenha sido adicionada na contsubf, variável que armazena as subformulas
-            if a not in contsubf:   
-                contsubf.append(a)
             a = a[1:-1]
 
             aux = 0
             i = 0
-
-            #Divide a string/fórmula em dois e analisa os parenteses
             while i < len(a)+1:
                     if a[i] == "(":
                         aux += 1
@@ -68,54 +46,43 @@ def principal(a, contsubf):
 
                     i += 1
             
-            #Partes da string/fórmula
             parte1 = a[0:x+1]
             parte2 = a[x+2:len(a)]
             
-            #Caso a negação esteja no fim da fórmula, sem um átomo depois
             if x == len(a)-1:
                 return "FALSO"
 
-            #Caso encontre um conectivo
             elif a[x+1] in CONECTIVOS:
-                return principal(parte1, contsubf) + a[x+1] + principal(parte2, contsubf)
+                return principal(parte1) + a[x+1] + principal(parte2)
 
-        #Senão for nenhum desses casos
         return "FALSO"
 
-#Função para verificar as subformulas repetidas
+
+### FUNÇÕES PARA VERIFICAR AS SUBFORMULAS
 def veriRepe(a, sub):
-    #Caso a fórmula ainda não foi adicionada na sub, variavel para armazenar as subformulas
     if a not in sub:
         return sub.append(a)
     else:
         return sub
 
-#Função para verificar as subformulas
+
 def veriSub(a):
     sub = []
     i = 0
 
-    #Percorre a string/fórmula
     while i < len(a):   
 
-        #Caso tenha encontrado uma átomo
         if a[i].isalpha():
             veriRepe(a[i], sub)
         
-        #Caso tenha encontrado um conectivo de negação
         if a[i] == "-":
-            #Verifica se o próximo é um átomo
             if a[i+1].isalpha():
                 veriRepe(a[i]+a[i+1], sub)
 
-            #Verifica se o próximo é uma abertura de parentese
             if a[i+1] == "(":
                 
                 j = i+2
                 aux = 1
-
-                #Divide a string/fórmula em dois e analisa os parenteses
                 while j < len(a):
                     if a[j] == "(":
                         aux += 1
@@ -127,10 +94,8 @@ def veriSub(a):
 
                     j += 1
 
-                #Verifica uma parte da string/fórmula
                 veriRepe(a[i:aux+1], sub)
             
-        #Caso tenha encontrado uma abertura de parentese
         if a[i] == "(":
                 
                 j = i+1
@@ -147,14 +112,12 @@ def veriSub(a):
                     j += 1
 
                 veriRepe(a[i:aux+1], sub)
-
         i += 1
+
     return(sub)
 
-#Função principal
 def main():
 
-    #Menu de apresentação
     print("#"*33)
     print("  Verificador de Fórmula Lógica  ")
     print("#"*33)
@@ -170,36 +133,24 @@ def main():
     print("Exemplo:\033[1;32m (-(a#b)>(a&b)) \033[m")
     print()
 
-    #Pedir uma fórmula proposicional ao usuário
-    formula = input("Digite uma formula: ")
+    formula = input("digite uma formula com ou sem espaços entre os caracteres: ")
 
-    #Tirando os espaços, se tiver, da fórmula
     semespaco = ""
     for i in range(len(formula)):
         if formula[i] != " ":
             semespaco = semespaco + formula[i]
 
-    #Criando a variável em formato de lista para armazenar as subformulas
-    contsubf = []
+    resultado = principal(semespaco)
 
-    #Mandando a fórmula para a verificação
-    resultado = principal(semespaco, contsubf)
-
-    #Após receber o resultado da verificação
     if "FALSO" in resultado:
-        #Se não for fórmula
         print("\n>>> NÃO É FORMULA")
 
     else:
 
-        #Se for fórmula
         listOrdem = sorted(veriSub(semespaco), key=len)
         print("\n>>> É FORMULA")
         print(">>> As subformulas são:", listOrdem)
         print(">>> A complexidade é:", len(resultado))
 
-#Armazendo os conectivos lógicos em uma só variável
 CONECTIVOS = [">" , "&", "#"]
-
-#Chamando a função main para começar o código
 main()
