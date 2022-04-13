272b859ed5406a1c8990273dfef719faed8bd88c
moisesocosta <mosolicos@gmail.com>
2022-04-13 09:13:15 -0300

    corrigindo pastas

--- Faculdade/WEB/T16/frontend/src/components/home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Main from "../template/Main";
-
-export default props =>
-    <Main icon="home" title="Início" subtitle="Modelo Projeto Web">
-        <div className="display-4">Seja Bem Vindo!</div>
-        <hr />
-        <p className="mb-0">Sistema de exemplo para a disciplina de Desenvolvimento Web</p>
-    </Main>
--- Faculdade/WEB/T16/frontend/src/components/template/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import './Footer.css'
-import React from 'react'
-
-export default props => 
-    <footer className="footer">
-        Desenvolvido com <i className="fa fa-heart text-danger"/>
-        por <strong>Rodrigo Valença</strong>
-    </footer>
